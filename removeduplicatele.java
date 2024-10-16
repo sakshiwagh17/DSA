b25324e1479b82d54fd971f3e67f5bf606f03d75
Sakshi Wagh <159275370+sakshiwagh17@users.noreply.github.com>
2024-10-16 22:26:28 +0530

    to remove duplicate number

---
+++ removeduplicatele.java
@@ -0,0 +1,15 @@
+public class removeduplicatele {
+    public int remove(int[] nums) {
+        if(nums.length==0){
+            return -1;
+        }
+        int unique=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=nums[unique]){
+                unique++;
+                nums[unique]=nums[i]; 
+            }
+        }
+        return unique+1;
+    }
+}
