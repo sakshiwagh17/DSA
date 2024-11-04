340261c670b6bb7a48af4d3f00e7e430e1745ac8
Sakshi Wagh <159275370+sakshiwagh17@users.noreply.github.com>
2024-11-04 20:36:38 +0530

    Reverse the string

---
+++ ReversedString.java
@@ -0,0 +1,17 @@
+public class ReversedString {
+        public static String reverseString(String s) {
+            // Reverse the string str
+            char[] charArr=s.toCharArray();
+            int left=0;
+            int right=charArr.length-1;
+            while(left<right){
+                char temp=charArr[left];
+                charArr[left]=charArr[right];
+                charArr[right]=temp;
+                left++;
+                right--;
+            }
+            return new String(charArr);  
+        }
+}
+
