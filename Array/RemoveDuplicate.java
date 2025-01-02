3f747ac4585fa08fc1083439f8ba8707ae5ec6fc
Sakshi Wagh <sakshikwagh17@gmail.com>
2025-01-02 19:43:19 +0530

    Initial commit

---
+++ Array/RemoveDuplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int unique = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[unique]) {
+                unique++;
+                nums[unique] = nums[i];
+
+            }
+        }
+        return unique + 1;
+    }
+}
