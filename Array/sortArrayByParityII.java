7a6e551c1239b5e4c6b67a1a2ef19b652bb31bae
Sakshi Wagh <159275370+sakshiwagh17@users.noreply.github.com>
2025-01-28 22:57:06 +0530

    sort Array By Parity II

---
+++ Array/sortArrayByParityII.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int n=nums.length;
+        int even=0;int odd=1;
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            if(nums[i]%2==0){
+                ans[even]=nums[i];
+                even+=2;
+            }else{
+                ans[odd]=nums[i];
+                odd+=2;
+            }
+        }
+        return ans;
+    }
+}
