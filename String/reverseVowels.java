6e6b705633e71b37e29baa27592d7716ca7f315a
Sakshi Wagh <159275370+sakshiwagh17@users.noreply.github.com>
2025-01-28 22:43:44 +0530

    reverse vowels.

---
+++ String/reverseVowels.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String reverseVowels(String s) {
+        char[] str=s.toCharArray();
+        int left=0;
+        int right=str.length-1;
+        String vowels="aeiouAEIOU";
+        while(left<right){
+            while(left<right&&vowels.indexOf(str[left])==-1){
+                left++;
+            }
+            while(left<right&&vowels.indexOf(str[right])==-1){
+                right--;
+            }
+            char temp=str[left];
+            str[left]=str[right];
+            str[right]=temp;
+            left++;
+            right--;
+        }
+        return new String(str);   
+    }
+}
