9408d6cf2708d13131f43d2dabc5beb32334acb0
Sakshi Wagh <159275370+sakshiwagh17@users.noreply.github.com>
2025-03-22 11:37:31 +0530

    Selection Sort

---
+++ Sorting/selectionSort.java
@@ -0,0 +1,16 @@
+class Solution {
+    void selectionSort(int[] arr) {
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+    }
+}
