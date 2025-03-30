cef4bba5dfa7039837ca6df585979f94d6e7e711
Sakshi Wagh <159275370+sakshiwagh17@users.noreply.github.com>
2025-03-30 12:56:34 +0530

    Maximum Depth of Binary Tree

--- Binary Trees/maxDepth.java
+++ Binary Trees/maxDepth.java
@@ -22,3 +22,13 @@ class Solution {
         return depth;
     }
 }
+
+//recursive 
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root== null) return 0;
+        int lh=maxDepth(root.left);
+        int rh=maxDepth(root.right);
+        return 1+Math.max(lh,rh);
+    }
+} 
